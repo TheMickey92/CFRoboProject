e6624f608df710d12602befdb23b15e74215f7b9
TheMotivation <marcel.braeuninger@gmail.com>
2015-01-04 21:04:57 +0100

    Testcode aus Console entfernt

--- ConnectFour.Console/ConnectFourConsole.cs
+++ ConnectFour.Console/ConnectFourConsole.cs
@@ -6,29 +6,29 @@ namespace ConnectFour.Console
     {
         static void Main(string[] args)
         {
-            int[,] oldGameField = new int[7, 6];
-            oldGameField[3, 5] = 2;
-            oldGameField[4, 5] = 1;
-            oldGameField[3, 4] = 2;
-            oldGameField[4, 4] = 1;
+            //int[,] oldGameField = new int[7, 6];
+            //oldGameField[3, 5] = 2;
+            //oldGameField[4, 5] = 1;
+            //oldGameField[3, 4] = 2;
+            //oldGameField[4, 4] = 1;
 
-            int[,] newGameField = new int[7, 6];
-            newGameField[3, 5] = 2;
-            newGameField[4, 5] = 1;
-            newGameField[3, 4] = 2;
-            newGameField[4, 4] = 1;
-            newGameField[4, 3] = 1;
+            //int[,] newGameField = new int[7, 6];
+            //newGameField[3, 5] = 2;
+            //newGameField[4, 5] = 1;
+            //newGameField[3, 4] = 2;
+            //newGameField[4, 4] = 1;
+            //newGameField[4, 3] = 1;
 
-            string jsonOldGameField = InputHandling.GetJsonFrom2DArray(oldGameField);
-            string jsonNewGameField = InputHandling.GetJsonFrom2DArray(newGameField);
+            //string jsonOldGameField = InputHandling.GetJsonFrom2DArray(oldGameField);
+            //string jsonNewGameField = InputHandling.GetJsonFrom2DArray(newGameField);
 
-            args = new[]
-            {
-                //"[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,1,1],[0,0,0,0,0,2],[0,0,0,0,0,0],[0,0,0,0,0,0]]",
-                //"[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,1,1],[0,0,0,2,0,2],[0,0,0,0,0,0],[0,0,0,0,0,0]]"
-                jsonOldGameField,
-                jsonNewGameField
-            };
+            //args = new[]
+            //{
+            //    //"[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,1,1],[0,0,0,0,0,2],[0,0,0,0,0,0],[0,0,0,0,0,0]]",
+            //    //"[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,1,1],[0,0,0,2,0,2],[0,0,0,0,0,0],[0,0,0,0,0,0]]"
+            //    jsonOldGameField,
+            //    jsonNewGameField
+            //};
 
             if (args.Length != 2)
             {
