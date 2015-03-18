d511ca2d9e3908134b4e9ae8dc65dd69de9a84d6
TheMotivation <marcel.braeuninger@gmail.com>
2015-03-18 18:54:42 +0100

    Added CheckWinResult class
    This class is meant to be the answer to the GameControl.CheckWin
    function. It saves who won and with which points the game was won,

---
+++ ConnectFour.Logic/CheckWinResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace ConnectFour.Logic
+{
+    public class CheckWinResult
+    {
+        public CheckWinResult(int winner, List<Point> points)
+        {
+            Points = points;
+            Winner = winner;
+        }
+
+        public CheckWinResult(int winner)
+        {
+            Winner = winner;
+            Points = new List<Point>();
+        }
+
+        public int Winner { get; private set; }
+        public List<Point> Points { get; private set; } 
+    }
+}
