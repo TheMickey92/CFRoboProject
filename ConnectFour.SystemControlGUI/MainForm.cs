1e4d9c8fb30c732d88e64d5d07c5722be93ada9e
TheMotivation <marcel.braeuninger@gmail.com>
2015-04-01 11:31:50 +0200

    Created SystemControlGUI project map

---
+++ ConnectFour.SystemControlGUI/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ConnectFour.SystemControlGUI
+{
+    public partial class MainForm : Form
+    {
+        private string pathToConsole = "";
+        public MainForm()
+        {
+            InitializeComponent();
+
+            string altPath = "../../../ConnectFour.Console/bin/Debug/CFConsole.exe";
+
+            if (File.Exists("CFConsole.exe"))
+                pathToConsole = "CFConsole.exe";
+            else if (File.Exists(altPath))
+                pathToConsole = altPath;
+            else
+            {
+                MessageBox.Show("CFConsole not found!");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
